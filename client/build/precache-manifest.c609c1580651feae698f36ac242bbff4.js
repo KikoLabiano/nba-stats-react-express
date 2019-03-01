d89df8c0860906e5e9fd9ae17f1f2d29d4ea8e19
D807354 <fj.labiano.jauregui@navarra.es>
2019-03-01 14:01:38 +0100

    Build

---
+++ client/build/precache-manifest.c609c1580651feae698f36ac242bbff4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "0d3164882ee8c160c352",
+    "url": "/static/js/main.0d316488.chunk.js"
+  },
+  {
+    "revision": "298d7220a07a60c13f1e",
+    "url": "/static/js/2.298d7220.chunk.js"
+  },
+  {
+    "revision": "0d3164882ee8c160c352",
+    "url": "/static/css/main.bf23c275.chunk.css"
+  },
+  {
+    "revision": "8bb7512abde5539dec0e5e3f91a06201",
+    "url": "/index.html"
+  }
+];
